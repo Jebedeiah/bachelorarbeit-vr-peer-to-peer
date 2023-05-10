537a18f560ff56132334fc005b6746802ff88865
Jeremy Camier <jecait01@hs-esslingen.de>
2023-05-10 02:07:40 +0200

    Raycaster hinzugefügt, anzeigen von anderen Spielern verbessert, Kugeln kommen jetzt aus Rohr und fliegen Richtung raycast intersection bzw. maximal zum Punkt der 200 EInheiten weit entfernt liegt.

--- js/host.js
+++ js/host.js
@@ -11,14 +11,20 @@ let obstacle, obstacle2, obstacle3, obstacle4;
 let bulletBB, obstacleBB;
 let bulletBBHelper;
 let dolly;
+let player = new THREE.Object3D();
+let playerWeapon;
 let otherPlayers = [];
 let otherPlayersWeapons = [];
+let otherPlayersBullets = [];
 let controller1, controller2;
 let controllerGrip1, controllerGrip2;
 let raycaster = new THREE.Raycaster();
-let bullet;
+let rayEndPoint = new THREE.Vector3();
+let maxRayDistance = 200;
+let objects = [];
+let collisionBoxes = [];
+let bullet = null;
 let gamepad;
-let xRotationQuaternion = new THREE.Quaternion();
 let tank = new THREE.Object3D();
 const clock = new THREE.Clock();
 
@@ -47,30 +53,32 @@ window.addEventListener('DOMContentLoaded', async () => {
 });
 
 // Three.js Code
-function init(){
+async function init(){
 
 	initPhysics();
 
 	// GLTF Loader
-	loader.load( 'public/models/tank.glb', function ( gltf ) {
-		tank = gltf.scene;
-		tank.traverse((obj) => {
-			if(obj.isMesh){
-					obj.material = new THREE.MeshStandardMaterial({color: 0x0096FF})					
-				}
-			}			
-		)
-		tank.position.set(0, .5, 0);
-	}, undefined, function ( error ) {
-		console.error( error );
-	} );
+	const loadedData = await loader.loadAsync( 'public/models/tank.glb');
+
+	tank = loadedData.scene;
+	tank.traverse((obj) => {
+		if(obj.isMesh){
+				obj.material = new THREE.MeshStandardMaterial({color: 0x0096FF})					
+			}
+		}			
+	)
+	tank.position.set(0, .5, 0);
+	player = tank.clone();
+	playerWeapon = player.children[1];
+	
+	// player.rotation.y = Math.PI;
 
 	
 	scene = new THREE.Scene();
 	camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 500 );
 	renderer = new THREE.WebGLRenderer();
 	renderer.setSize( window.innerWidth, window.innerHeight );
-	renderer.outputEncoding = THREE.sRGBEncoding;
+	renderer.outputColorSpace = THREE.SRGBColorSpace;
 	document.body.appendChild( renderer.domElement );
 	document.body.appendChild( VRButton.createButton( renderer ) );
 	renderer.xr.enabled = true;
@@ -87,9 +95,13 @@ function init(){
 	// Add floor
 	const ground = new THREE.Mesh(
 		new THREE.BoxGeometry(100, 1, 100),
-		new THREE.MeshStandardMaterial({ color: 0x000000 }));
+		new THREE.MeshStandardMaterial({ color: 0x222222 }));
 	ground.castShadow = false;
 	ground.receiveShadow = true;
+	const groundBB = new THREE.Box3().setFromObject(ground);
+	collisionBoxes.push(groundBB);
+
+	objects.push(ground);
  	scene.add(ground);
 
 	const rbGround = new RigidBody();
@@ -97,30 +109,39 @@ function init(){
 	physicsWorld.addRigidBody(rbGround.body);
 
 	// Add walls
-	const geometry = new THREE.PlaneGeometry( 100, 10 );
+	const geometry = new THREE.BoxGeometry( 100, 10, 2 );
 	const material = new THREE.MeshBasicMaterial( {color: 0x999999, side: THREE.DoubleSide} );
 	wall = new THREE.Mesh( geometry, material );
 	wall2 = wall.clone();
 	wall3 = wall.clone();
 	wall4 = wall.clone();
 	wall.position.set(0, 5, -50);
+	const wallBB = new THREE.Box3().setFromObject(wall);
+	collisionBoxes.push(wallBB);
 	wall2.position.set(-50, 5, 0);
 	wall2.rotateY(Math.PI / 2);
+	const wall2BB = new THREE.Box3().setFromObject(wall2);
+	collisionBoxes.push(wall2BB);
 	wall3.position.set(0, 5, 50);
+	const wall3BB = new THREE.Box3().setFromObject(wall3);
+	collisionBoxes.push(wall3BB);
 	wall4.position.set(50, 5, 0);
 	wall4.rotateY(Math.PI / 2);
-	
+	const wall4BB = new THREE.Box3().setFromObject(wall4);
+	collisionBoxes.push(wall4BB);
+	objects.push(wall, wall2, wall3, wall4)
 	scene.add( wall, wall2, wall3, wall4 );
-
+	
 	// Add obstacles
 	obstacle = new THREE.Mesh(new THREE.BoxGeometry(10, 10, 10), new THREE.MeshStandardMaterial({ color: 0x11ff11 }));
 	obstacle.position.set(-30, 5, -20);
 	obstacle.castShadow = true;
-
+	objects.push(obstacle);
+	
 	obstacleBB = new THREE.Box3().setFromObject(obstacle);
+	collisionBoxes.push(obstacleBB);
 
  	scene.add(obstacle);
-	scene.add(obstacleBB);
 	
 	// TestBox
 	const box = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshStandardMaterial({ color: 0x11ff11 }));
@@ -135,6 +156,8 @@ function init(){
 	
 	rigidBodies.push({mesh: box, rigidBody: rbBox});
 	
+	// Raycaster maximum Distanz entspricht Größe der Karte
+	raycaster.far = maxRayDistance;
 
 	// controllers
 	function onSelectStart() {
@@ -160,7 +183,7 @@ function init(){
 	controller2.addEventListener( 'selectstart', onSelectStart );
 	controller2.addEventListener( 'selectend', onSelectEnd );
 	controller2.addEventListener( 'connected', function ( event ) {
-		this.add( buildController() );
+		// this.add( raycaster );
 	});
 
 	controller2.addEventListener( 'disconnected', function () {
@@ -189,8 +212,7 @@ function init(){
     dolly.add(controller2);
     dolly.add(controllerGrip1);
     dolly.add(controllerGrip2);
-
-	xRotationQuaternion.setFromAxisAngle(new THREE.Vector3(-1, 0, 0), Math.PI / 4.71);
+	dolly.add(player);
 
 	animate();
 }
@@ -203,10 +225,19 @@ function buildController() {
 	geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( [ 0, 0, 0, 0, 0, - 1 ], 3 ) );
 	geometry.setAttribute( 'color', new THREE.Float32BufferAttribute( [ 0.5, 0.5, 0.5, 0, 0, 0 ], 3 ) );
 
-	material = new THREE.LineBasicMaterial( { vertexColors: true, blending: THREE.AdditiveBlending } );
+	material = new THREE.LineBasicMaterial( { vertexColors: true, blending: THREE.AdditiveBlending } );	
 
 	return new THREE.Line( geometry, material );
 }
+
+// function buildRaycasterLine(){
+// 	// Raycaster
+// 	const rayMaterial = new THREE.LineBasicMaterial({ color: 0xff0000 });
+// 	const rayGeometry = new THREE.BufferGeometry().setFromPoints([new THREE.Vector3(0,0,0), new THREE.Vector3(0,0,-1)]);
+// 	rayLine = new THREE.Line(rayGeometry, rayMaterial);
+
+// 	return rayLine;
+// }
 
 function updateOrientation() {
 	// Get joystick input		
@@ -234,8 +265,8 @@ function animate() {
 function render(){
 	const deltaTime = clock.getDelta();
 	updateOrientation();
+	updateRaycaster()
 	shoot();
-	moveBullet();
 	sendPlayerData();
 	updatePhysics(deltaTime);	
 	renderer.render( scene, camera );
@@ -266,42 +297,92 @@ const receiveData = () => {
 	for(let i = 0; i < conns.length; i++){
 		
 		conns[i].on('data', (data) => {	
-			//other players current position and looking direction
+			// other players current position and looking direction
 			otherPlayers[i].position.set(data[0].x, 0.5, data[0].z);
-			otherPlayers[i].rotation.y = data[1];
+			otherPlayers[i].rotation.y = data[1];			
+			// otherPlayers[i].rotation.y += Math.PI;
 
-			//other players weapon direction (based on their controller rotation)
+			// other players weapon direction (based on their controller rotation)
 			otherPlayersWeapons[i].position.set(data[0].x, 1.12, data[0].z);
-			otherPlayersWeapons[i].quaternion.set(data[3]._x, data[3]._y, data[3]._z, data[3]._w);
-			otherPlayersWeapons[i].quaternion.multiply(xRotationQuaternion);
+			// otherPlayersWeapons[i].quaternion.set(data[3]._x, data[3]._y, data[3]._z, data[3]._w);
+			otherPlayersWeapons[i].lookAt(new THREE.Vector3(data[3].x, data[3].y, data[3].z));
+			// otherPlayersWeapons[i].rotation.y += Math.PI;
+
+			// bullets shot from other players
+			if (data[4] === null) {
+				if (otherPlayersBullets[i]) {
+					scene.remove(otherPlayersBullets[i]);
+					otherPlayersBullets[i] = null;
+				}
+			} else {
+				if (!otherPlayersBullets[i]) {
+					const geometry = new THREE.SphereGeometry(0.1, 8, 4);
+					const material = new THREE.MeshStandardMaterial( { color: 0xffff00 } ); 
+					let otherBullet = new THREE.Mesh( geometry, material );
+					otherBullet.position.set(data[4].x, data[4].y, data[4].z);
+					otherPlayersBullets[i] = otherBullet;
+					scene.add(otherPlayersBullets[i]);
+				} else {
+				  	otherPlayersBullets[i].position.set(data[4].x, data[4].y, data[4].z);
+				}
+			}			
 		});
+	}
+}
+
+// Get raycaster rayEndPoint
+function updateRaycaster() {
+	
+	for(let obj of objects){		
+		let pos = new THREE.Vector3();
+		let dir = new THREE.Vector3();
+		controller2.getWorldPosition(pos);
+		controller2.getWorldDirection(dir)
+		dir.multiplyScalar(-1);
+		raycaster.set(pos, dir);
+
+		const intersects = raycaster.intersectObject(obj);
+		if(intersects.length > 0){
+			rayEndPoint = intersects[0].point;
+		}
+		else{
+			rayEndPoint.addVectors(pos, dir.multiplyScalar(maxRayDistance) );
+		}	
+		if(playerWeapon){			
+			playerWeapon.lookAt(rayEndPoint);
+		}
 	}
 }
 
 const shoot = () => {
 	if ( controller2.userData.isSelecting === true ) {
 		if(!bullet){
-			const geometry = new THREE.SphereGeometry(0.2, 8, 4);
+			const geometry = new THREE.SphereGeometry(0.1, 8, 4);
 			const material = new THREE.MeshStandardMaterial( { color: 0xffff00 } ); 
 			bullet = new THREE.Mesh( geometry, material );
 			bullet.castShadow = true;
-			bullet.position.copy(controllerGrip2.position);
-			bullet.quaternion.copy(controllerGrip2.quaternion.multiply(xRotationQuaternion));
-
+			bullet.position.z += 1
+			bullet.position.y += .1
 			bulletBB = new THREE.Box3().setFromObject(bullet);
 			bulletBBHelper = new THREE.Box3Helper(bulletBB);
 
-			scene.add( bullet );
-			scene.add( bulletBB );
-			scene.add( bulletBBHelper );
+			playerWeapon.add( bullet );
+			// scene.add( bulletBBHelper );
 		}
 	}
+	moveBullet();
 }
 
 const moveBullet = () => {
 	if(bullet){
-		bullet.translateZ(-0.3);
+		bullet.translateZ(.5);
 		bulletBB.setFromObject(bullet);
+		for(let box of collisionBoxes){
+			if(bulletBB.intersectsBox(box)){
+				scene.remove(bullet);
+				bullet = null;
+			}			
+		}
 	}
 }
 
@@ -315,20 +396,24 @@ const sendPlayerData = () => {
 	if(conns.length > 0){
 		let controllerPos = new THREE.Vector3();
 		let cameraPos = new THREE.Vector3();
-		let controllerQuat = new THREE.Quaternion();
+		// let controllerQuat = new THREE.Quaternion();
 		let cameraQuat = new THREE.Quaternion();
-		controllerGrip2.getWorldPosition(controllerPos);
-		controllerGrip2.getWorldQuaternion(controllerQuat);
+		let bulletPos = new THREE.Vector3();
+		controller2.getWorldPosition(controllerPos);
+		// controller2.getWorldQuaternion(controllerQuat);
 		dummyCam.getWorldPosition(cameraPos);
 		dummyCam.getWorldQuaternion(cameraQuat);
+		if(bullet){
+			bullet.getWorldPosition(bulletPos);
+		} else{ bulletPos = null;}
 
 		// Get only Y-Rotation from Quaternion
 		const euler = new THREE.Euler(0, 0, 0, 'YXZ');
 		euler.setFromQuaternion(cameraQuat, 'YXZ');
-		let posQuatData = [cameraPos, euler.y, controllerPos, controllerQuat];
+		let playerData = [cameraPos, euler.y, controllerPos, rayEndPoint, bulletPos];
 
 		for(let conn of conns){
-			conn.send(posQuatData);
+			conn.send(playerData);
 		}
 	}	
 }
@@ -342,9 +427,9 @@ const createOtherPlayer = () => {
 
 	// Create other Player's weapon in Scene
 	// const oppWepMaterial = new THREE.MeshStandardMaterial( {color: 0x00ffff} );
-	let opponentsWeapon = opponent.children[1];
-	scene.add(opponentsWeapon);
+	let opponentsWeapon = opponent.children[1];	
 	otherPlayersWeapons.push(opponentsWeapon);
+	scene.add(opponentsWeapon);
 }
 
 
--- js/join.js
+++ js/join.js
@@ -6,16 +6,26 @@ import { XRControllerModelFactory } from 'three/addons/webxr/XRControllerModelFa
 
 //Three variables
 let camera, dummyCam, scene, renderer;
+let wall, wall2, wall3, wall4;
+let obstacle, obstacle2, obstacle3, obstacle4;
+let bulletBB, obstacleBB;
+let bulletBBHelper;
 let dolly;
+let player = new THREE.Object3D();
+let playerWeapon;
 let otherPlayers = [];
 let otherPlayersWeapons = [];
+let otherPlayersBullets = [];
 let controller1, controller2;
 let controllerGrip1, controllerGrip2;
+let raycaster = new THREE.Raycaster();
+let rayEndPoint = new THREE.Vector3();
+let maxRayDistance = 200;
+let objects = [];
+let collisionBoxes = [];
+let bullet = null;
 let gamepad;
-let xRotationQuaternion = new THREE.Quaternion();
 let tank = new THREE.Object3D();
-let tankTower = new THREE.Object3D();
-let tankBody = new THREE.Object3D();
 const clock = new THREE.Clock();
 
 // Physics variables
@@ -45,36 +55,31 @@ window.addEventListener('DOMContentLoaded', async () => {
 });
 
 // Three.js Code
-function init(){	
+async function init(){
 
 	initPhysics();
 
 	// GLTF Loader
-	loader.load( 'public/models/tank.glb', function ( gltf ) {
-		tank = gltf.scene;
-		tank.traverse((obj) => {
-			if(obj.isMesh){
-					obj.material = new THREE.MeshStandardMaterial({color: 0x0096FF})
-	if(obj.name === "Lower"){
-						tankBody = obj;
-					}
-					else if(obj.name === "Top"){
-						tankTower = obj;
-					}
-				}
+	const loadedData = await loader.loadAsync( 'public/models/tank.glb');
+
+	tank = loadedData.scene;
+	tank.traverse((obj) => {
+		if(obj.isMesh){
+				obj.material = new THREE.MeshStandardMaterial({color: 0x0096FF})					
 			}
-		)
-		tank.position.set(0, .5, 0);
-	}, undefined, function ( error ) {
-		console.error( error );
-	} );
+		}			
+	)
+	tank.position.set(0, .5, 0);
+	player = tank.clone();
+	playerWeapon = player.children[1];
+	// player.rotation.y = Math.PI;
 
 
 	scene = new THREE.Scene();
 	camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 500 );
 	renderer = new THREE.WebGLRenderer();
 	renderer.setSize( window.innerWidth, window.innerHeight );
-	renderer.outputEncoding = THREE.sRGBEncoding;
+	renderer.outputColorSpace = THREE.SRGBColorSpace;
 	document.body.appendChild( renderer.domElement );
 	document.body.appendChild( VRButton.createButton( renderer ) );
 	renderer.xr.enabled = true;
@@ -91,15 +96,54 @@ function init(){
 	// Add floor
 	const ground = new THREE.Mesh(
 		new THREE.BoxGeometry(100, 1, 100),
-		new THREE.MeshStandardMaterial({ color: 0x000000 }));
+		new THREE.MeshStandardMaterial({ color: 0x222222 }));
 	ground.castShadow = false;
 	ground.receiveShadow = true;
+	const groundBB = new THREE.Box3().setFromObject(ground);
+	collisionBoxes.push(groundBB);
+
+	objects.push(ground);
  	scene.add(ground);
 
 	const rbGround = new RigidBody();
 	rbGround.createBox(0, ground.position, ground.quaternion, new THREE.Vector3(100, 1, 100));
 	physicsWorld.addRigidBody(rbGround.body);
 
+	// Add walls
+	const geometry = new THREE.BoxGeometry( 100, 10, 2 );
+	const material = new THREE.MeshBasicMaterial( {color: 0x999999, side: THREE.DoubleSide} );
+	wall = new THREE.Mesh( geometry, material );
+	wall2 = wall.clone();
+	wall3 = wall.clone();
+	wall4 = wall.clone();
+	wall.position.set(0, 5, -50);
+	const wallBB = new THREE.Box3().setFromObject(wall);
+	collisionBoxes.push(wallBB);
+	wall2.position.set(-50, 5, 0);
+	wall2.rotateY(Math.PI / 2);
+	const wall2BB = new THREE.Box3().setFromObject(wall2);
+	collisionBoxes.push(wall2BB);
+	wall3.position.set(0, 5, 50);
+	const wall3BB = new THREE.Box3().setFromObject(wall3);
+	collisionBoxes.push(wall3BB);
+	wall4.position.set(50, 5, 0);
+	wall4.rotateY(Math.PI / 2);
+	const wall4BB = new THREE.Box3().setFromObject(wall4);
+	collisionBoxes.push(wall4BB);
+	objects.push(wall, wall2, wall3, wall4)	
+	scene.add( wall, wall2, wall3, wall4 );
+	
+	// Add obstacles
+	obstacle = new THREE.Mesh(new THREE.BoxGeometry(10, 10, 10), new THREE.MeshStandardMaterial({ color: 0x11ff11 }));
+	obstacle.position.set(-30, 5, -20);
+	obstacle.castShadow = true;
+	objects.push(obstacle);
+	
+	obstacleBB = new THREE.Box3().setFromObject(obstacle);
+	collisionBoxes.push(obstacleBB);
+
+ 	scene.add(obstacle);
+	
 	// TestBox
 	const box = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshStandardMaterial({ color: 0x11ff11 }));
 	box.position.set(0, 10, -20);
@@ -113,6 +157,8 @@ function init(){
 	
 	rigidBodies.push({mesh: box, rigidBody: rbBox});
 
+	// Raycaster maximum Distanz entspricht Größe der Karte
+	raycaster.far = maxRayDistance;
 	
 	// controllers
 	function onSelectStart() {
@@ -138,7 +184,7 @@ function init(){
 	controller2.addEventListener( 'selectstart', onSelectStart );
 	controller2.addEventListener( 'selectend', onSelectEnd );
 	controller2.addEventListener( 'connected', function ( event ) {
-		this.add( buildController() );
+		// this.add( raycaster );
 	});
 
 	controller2.addEventListener( 'disconnected', function () {
@@ -167,8 +213,7 @@ function init(){
     dolly.add(controller2);
     dolly.add(controllerGrip1);
     dolly.add(controllerGrip2);
-
-	xRotationQuaternion.setFromAxisAngle(new THREE.Vector3(-1, 0, 0), Math.PI / 4.71);
+	dolly.add(player);
 
 	animate();
 }
@@ -212,6 +257,8 @@ function animate() {
 function render(){
 	const deltaTime = clock.getDelta();
 	updateOrientation();
+	updateRaycaster()
+	shoot();
 	sendPlayerData();
 	updatePhysics(deltaTime);
 	renderer.render( scene, camera );
@@ -263,28 +310,95 @@ const receiveData = () => {
 	for(let i = 0; i < conns.length; i++){
 		
 		conns[i].on('data', (data) => {	
-			//other players current position and looking direction
+			// other players current position and looking direction
 			otherPlayers[i].position.set(data[0].x, 0.5, data[0].z);
 			otherPlayers[i].rotation.y = data[1];
+			// otherPlayers[i].rotation.y += Math.PI;
 
-			//other players weapon direction (based on their controller rotation)
+			// other players weapon direction (based on their controller rotation)
 			otherPlayersWeapons[i].position.set(data[0].x, 1.12, data[0].z);
-			otherPlayersWeapons[i].quaternion.set(data[3]._x, data[3]._y, data[3]._z, data[3]._w);
-			otherPlayersWeapons[i].quaternion.multiply(xRotationQuaternion);
+			// otherPlayersWeapons[i].quaternion.set(data[3]._x, data[3]._y, data[3]._z, data[3]._w);
+			otherPlayersWeapons[i].rotation.y = data[1];
+			otherPlayersWeapons[i].lookAt(new THREE.Vector3(data[3].x, data[3].y, data[3].z));
+			// otherPlayersWeapons[i].rotation.y += Math.PI;
+
+			// bullets shot from other players
+			if (data[4] === null) {
+				if (otherPlayersBullets[i]) {
+				  	scene.remove(otherPlayersBullets[i]);
+				  	otherPlayersBullets[i] = null;
+				}
+			} else {
+				if (!otherPlayersBullets[i]) {
+					const geometry = new THREE.SphereGeometry(0.1, 8, 4);
+					const material = new THREE.MeshStandardMaterial( { color: 0xffff00 } ); 
+					let otherBullet = new THREE.Mesh( geometry, material );
+					otherBullet.position.set(data[4].x, data[4].y, data[4].z);
+					otherPlayersBullets[i] = otherBullet;
+					scene.add(otherPlayersBullets[i]);
+				} else {
+				  	otherPlayersBullets[i].position.set(data[4].x, data[4].y, data[4].z);
+				}
+			}
 		});
 	}
 }
 
-//Answer incoming connection
-peer.on('connection', (conn) => {
-	conn.on('open', () => {
-		remotePeers.push(conn.peer);		
-		console.log("Connection established with " + conn.peer);
-		createOtherPlayer();
-		conns.push(conn);
-		receiveData();
-	});
-});
+// Get raycaster rayEndPoint
+function updateRaycaster() {
+	
+	for(let obj of objects){
+		let pos = new THREE.Vector3();
+		let dir = new THREE.Vector3();
+		controller2.getWorldPosition(pos);
+		controller2.getWorldDirection(dir)
+		dir.multiplyScalar(-1);
+		raycaster.set(pos, dir);
+
+		const intersects = raycaster.intersectObject(obj);
+		if(intersects.length > 0){
+			rayEndPoint = intersects[0].point;
+		}
+		else{
+			rayEndPoint.addVectors(pos, dir.multiplyScalar(maxRayDistance) );
+		}	
+		if(playerWeapon){
+			playerWeapon.lookAt(rayEndPoint);
+		}
+	}
+}
+
+const shoot = () => {
+	if ( controller2.userData.isSelecting === true ) {
+		if(!bullet){
+			const geometry = new THREE.SphereGeometry(0.1, 8, 4);
+			const material = new THREE.MeshStandardMaterial( { color: 0xffff00 } ); 
+			bullet = new THREE.Mesh( geometry, material );
+			bullet.castShadow = true;
+			bullet.position.z += 1
+			bullet.position.y += .1
+			bulletBB = new THREE.Box3().setFromObject(bullet);
+			bulletBBHelper = new THREE.Box3Helper(bulletBB);
+
+			playerWeapon.add( bullet );
+			// scene.add( bulletBBHelper );
+		}
+	}
+	moveBullet();
+}
+
+const moveBullet = () => {
+	if(bullet){
+		bullet.translateZ(.5);
+		bulletBB.setFromObject(bullet);
+		for(let box of collisionBoxes){
+			if(bulletBB.intersectsBox(box)){
+				scene.remove(bullet);
+				bullet = null;
+			}			
+		}
+	}
+}
 
 peer.on('error', function (err) {
 	console.log(err);
@@ -296,19 +410,24 @@ const sendPlayerData = () => {
 	if(conns.length > 0){
 		let controllerPos = new THREE.Vector3();
 		let cameraPos = new THREE.Vector3();
-		let controllerQuat = new THREE.Quaternion();
+		// let controllerQuat = new THREE.Quaternion();
 		let cameraQuat = new THREE.Quaternion();
-		controllerGrip2.getWorldPosition(controllerPos);
-		controllerGrip2.getWorldQuaternion(controllerQuat);
+		let bulletPos = new THREE.Vector3();
+		controller2.getWorldPosition(controllerPos);
+		// controller2.getWorldQuaternion(controllerQuat);
 		dummyCam.getWorldPosition(cameraPos);
 		dummyCam.getWorldQuaternion(cameraQuat);
+		if(bullet){
+			bullet.getWorldPosition(bulletPos);
+		} else{ bulletPos = null;}
+		
+		// Get only Y-Rotation from Quaternion
 		const euler = new THREE.Euler(0, 0, 0, 'YXZ');
 		euler.setFromQuaternion(cameraQuat, 'YXZ');
-		console.log(euler.y);
-		let posQuatData = [cameraPos, euler.y, controllerPos, controllerQuat];
+		let playerData = [cameraPos, euler.y, controllerPos, rayEndPoint, bulletPos];
 
 		for(let conn of conns){
-			conn.send(posQuatData);
+			conn.send(playerData);
 		}
 	}	
 }
